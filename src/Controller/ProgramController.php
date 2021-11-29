4c4c509ff20dbe4b3c91714824e451f957560d24
Chambre ryan <chambre.ryan@orange.fr>
2021-11-29 17:26:31 +0100

    add some style but it's uggly

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,19 +18,39 @@ class ProgramController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('program.html.twig', [
-                'message' => 'Wild Series'
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+        return $this->render('program/index.html.twig', [
+                'programs' => $programs
             ]
         );
     }
 
     /**
-     * @Route("/program/{id}/", methods={"GET"}, requirements={"id"="\d+"}, name="program_show", )
+
+     * Getting a program by id
+
+     *
+
+     * @Route("/show/{id<^[0-9]+$>}", name="program_show")
+
+     * @return Response
+
      */
     public function show($id): Response
     {
-        return $this->render('programShow.html.twig', [
-                'message' => $id
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '. $id . ' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', [
+                'program' => $program
             ]
         );
     }
